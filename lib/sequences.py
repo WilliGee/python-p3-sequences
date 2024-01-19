ec194f752c7b6e78f2be13a46922fa703af17871
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2024-01-20 00:06:32 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,15 @@
 
 def print_fibonacci(length):
     pass
+    sequence = []
+
+    if length > 0:
+        sequence.append(0)
+
+    if length >= 2:
+        sequence.append(1)
+
+        for i in range(2,length):
+            sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)
